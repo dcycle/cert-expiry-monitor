23f700502f081ee344f1e94483a14a111901f0d1
alberto56 <albert@mediatribe.net>
2020-03-25 16:01:39 -0400

    Please squash this commit in the pull request

--- script/modules/check/commands/check/convert.source.sh
+++ script/modules/check/commands/check/convert.source.sh
@@ -1,1 +1,6 @@
-CONVERTEDDATE=$(date     -jf "%b %d %H:%M:%S %Y %Z"     -v -20S     "$DATETOCONVERT"     "+%s")
+if hash sw_vers 2>/dev/null; then
+  # On mac OS
+  CONVERTEDDATE=$(date -jf "%b %d %H:%M:%S %Y %Z" -v -20S "$DATETOCONVERT" "+%s")
+else
+  CONVERTEDDATE=$(date "+%s" -d "$DATETOCONVERT")
+fi
