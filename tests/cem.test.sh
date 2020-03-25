b7604a1a008767c838a5d9bd30d029f81ec26140
Albert Albala <albert@mediatribe.net>
2020-03-25 17:15:40 -0400

    Use different date command on mac OS and on Linux (#1)

--- tests/cem.test.sh
+++ tests/cem.test.sh
@@ -1,6 +1,6 @@
 set -e
 
-RESULT=$(./example-script.sh internal-test /tests/example-script/happy)
+RESULT=$(./cem.sh internal-test /tests/cem/happy)
 
 echo "$RESULT" | grep 'MAIN SCRIPT CALLED' > /dev/null
 echo "$RESULT" | grep 'INVOKE CALLED' > /dev/null
@@ -9,7 +9,7 @@ echo "$RESULT" | grep '\-\-internalscriptdir=' > /dev/null
 echo "$RESULT" | grep '\-\-internalcalldir=' > /dev/null
 echo "$RESULT" | grep '\-\-internalrunid=' > /dev/null
 
-RESULT=$(./example-script.sh internal-test /tests/example-script/sad) || FAIL=1
+RESULT=$(./cem.sh internal-test /tests/cem/sad) || FAIL=1
 
 # even in the case of a failure we clean up before quitting.
 echo "$RESULT" | grep 'INVOKE CALLED' > /dev/null
