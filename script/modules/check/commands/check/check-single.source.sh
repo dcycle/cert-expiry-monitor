f10f93b3a999734d6c7ba3b65269b549b11850dc
Albert Albala <albert@mediatribe.net>
2022-11-15 08:36:21 -0500

    Do not break the entire process if one site has no certificate at all (#2)

--- script/modules/check/commands/check/check-single.source.sh
+++ script/modules/check/commands/check/check-single.source.sh
@@ -1,16 +1,20 @@
-DATE=$(echo | openssl s_client -servername "$SITE" -connect "$SITE":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter)
-DATE=$(printf '%s\n' "${DATE//notAfter=/}")
-DATETOCONVERT="$DATE"
-source ./script/modules/check/commands/check/convert.source.sh
-EXP=$CONVERTEDDATE
-source ./script/modules/check/commands/check/today.source.sh
-DATETOCONVERT="$TODAY"
-source ./script/modules/check/commands/check/convert.source.sh
-DIFF=$(echo "($EXP-$CONVERTEDDATE)/60/60/24"|bc)
-echo "$SITE expires in $DIFF days, on $DATE"
-if [ "$DIFF" -lt "$DAYS" ]; then
-  if [ ! -z "$ERR" ];then
-    ERR=$ERR";"
-  fi;
-  ERR=$ERR"$SITE expires in $DIFF days"
+DATE=$(echo | openssl s_client -servername "$SITE" -connect "$SITE":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter) || ERR="NOCERT"
+if [ "$ERR" == "NOCERT" ]; then
+  ERR="$SITE does not seem to have a certificate at all"
+else
+  DATE=$(printf '%s\n' "${DATE//notAfter=/}")
+  DATETOCONVERT="$DATE"
+  source ./script/modules/check/commands/check/convert.source.sh
+  EXP=$CONVERTEDDATE
+  source ./script/modules/check/commands/check/today.source.sh
+  DATETOCONVERT="$TODAY"
+  source ./script/modules/check/commands/check/convert.source.sh
+  DIFF=$(echo "($EXP-$CONVERTEDDATE)/60/60/24"|bc)
+  echo "$SITE expires in $DIFF days, on $DATE"
+  if [ "$DIFF" -lt "$DAYS" ]; then
+    if [ ! -z "$ERR" ];then
+      ERR=$ERR";"
+    fi;
+    ERR=$ERR"$SITE expires in $DIFF days"
+  fi
 fi
