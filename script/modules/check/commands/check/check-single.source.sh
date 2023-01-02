da75d0254bd0ea2ba182a38ccf142bc965d39695
Albert Albala <albert@mediatribe.net>
2023-01-01 21:23:49 -0500

    Report errors to correct streams, and show all errors after first error (#4)

--- script/modules/check/commands/check/check-single.source.sh
+++ script/modules/check/commands/check/check-single.source.sh
@@ -1,18 +1,12 @@
+MYERR=""
 DATE=$(echo | openssl s_client -servername "$SITE" -connect "$SITE":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter) || MYERR="NOCERT"
 
 if [ "$MYERR" == "NOCERT" ]; then
-  echo "EEEEE"
-  if [ ! -z "$ERR" ];then
-    ERR=$ERR";"
-  fi;
-  ERR=$ERR"$SITE does not seem to have a certificate at all"
+  ERR=$ERR";$SITE does not seem to have a certificate at all"
 else
-  curl -I https://"$SITE" || MYERR="NOCURL"
+  curl -I https://"$SITE" >/dev/null 2>/dev/null || MYERR="NOCURL"
   if [ "$MYERR" == "NOCURL" ]; then
-    if [ ! -z "$ERR" ];then
-      ERR=$ERR";"
-    fi;
-    ERR=$ERR"$SITE could not be reached via curl"
+    ERR=$ERR";$SITE could not be reached via curl"
   else
     DATE=$(printf '%s\n' "${DATE//notAfter=/}")
     DATETOCONVERT="$DATE"
@@ -24,10 +18,7 @@ else
     DIFF=$(echo "($EXP-$CONVERTEDDATE)/60/60/24"|bc)
     echo "$SITE expires in $DIFF days, on $DATE"
     if [ "$DIFF" -lt "$DAYS" ]; then
-      if [ ! -z "$ERR" ];then
-        ERR=$ERR";"
-      fi;
-      ERR=$ERR"$SITE expires in $DIFF days"
+      ERR=$ERR";$SITE expires in $DIFF days"
     fi;
   fi;
 fi;
